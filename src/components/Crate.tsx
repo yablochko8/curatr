50646d04342469b6371906658e7db84bac4318ce
Lui <6627294+yablochko8@users.noreply.github.com>
2025-04-25 12:50:40 +0100

    don't crash on last guess

--- src/components/Crate.tsx
+++ src/components/Crate.tsx
@@ -5,16 +5,18 @@ export const Crate = ({ notSorted, onGuess }: { notSorted: Artwork[], onGuess: (
     return (
         <div className="flex flex-row w-full border-t-2 border-black p-8 justify-center">
             {/* Show the next image really big, with buttons below it */}
-            <div className="flex flex-col items-center">
-                <div className="flex flex-row gap-4">
-                    <button onClick={() => onGuess(notSorted[0], "left")}>Left</button>
-                    <button onClick={() => onGuess(notSorted[0], "right")}>Right</button>
+            {notSorted.length > 0 && (
+                <div className="flex flex-col items-center">
+                    <div className="flex flex-row gap-4">
+                        <button onClick={() => onGuess(notSorted[0], "left")}>Left</button>
+                        <button onClick={() => onGuess(notSorted[0], "right")}>Right</button>
+                    </div>
+                    <img className="h-full p-4" src={notSorted[0].imageUrl} alt={notSorted[0].title} />
                 </div>
-                <img className="h-full p-4" src={notSorted[0].imageUrl} alt={notSorted[0].title} />
-            </div>
+            )}
             {/* Show the rest of the images in a grid, with no buttons */}
             <div className="flex flex-col p-8 gap-4">
-                {notSorted.map((artwork) => (
+                {notSorted.length > 1 && notSorted.map((artwork) => (
                     <CrateQueueItem artwork={artwork} />
                 ))}
             </div>
