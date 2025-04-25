b4e210ebc4574a3f3a665f26edd66599efc28e09
Lui <6627294+yablochko8@users.noreply.github.com>
2025-04-25 12:05:43 +0100

    wip

--- src/components/StartButton.tsx
+++ src/components/StartButton.tsx
@@ -2,11 +2,30 @@ import { GameState } from "../types"
 
 export const StartButton = ({ gameState, setGameState }: { gameState: GameState, setGameState: (gameState: GameState) => void }) => {
 
-    return <button onClick={() => setGameState({
-        ...gameState,
-        currentRound: null,
-        rounds: [],
-        totalPoints: 0,
-        isGameOver: false
-    })}>Start Game</button>
+    const handleClick = () => {
+        console.log("handleClick", gameState)
+
+        // Identify which is the next round that has not been played
+        const hasPlayed = gameState.rounds.some((round) => round.userGuess)
+        const firstRound = gameState.rounds[0]
+        const nextRound = hasPlayed ? gameState.rounds.find((round) => !round.userGuess) : firstRound
+
+        if (nextRound) {
+            console.log("nextRound found", nextRound)
+            setGameState({
+                ...gameState,
+                currentRound: nextRound,
+            })
+        }
+        else {
+            console.log("no nextRound found")
+            setGameState({
+                ...gameState,
+                isGameOver: true
+            })
+            return
+        }
+    }
+
+    return <button onClick={handleClick}>Start Game</button>
 }
